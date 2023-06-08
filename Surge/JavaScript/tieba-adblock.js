6af5d4461aa3bf0f9e580729212cfdc1b23885fc
1o1lipop <66349154+1o1lipop@users.noreply.github.com>
2023-06-08 09:24:48 +0800

    Delete Surge/JavaScript directory

--- Surge/JavaScript/tieba-adblock.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-
-// 去除贴吧首页的广告
-body = body.replace(/<div id="thread_list_bottom_ad"[^>]*>[\s\S]*?<\/div>/g, "");
-
-// 去除帖子内容中的广告
-body = body.replace(/<div class="ad_[^>]*>[\s\S]*?<\/div>/g, "");
-
-$done({ body });
